4b346acdb38c68106ac8d2ad2b43fbb3c9641cf7
harold <piotr.e.wolny+pergit@gmail.com>
2014-05-09 20:46:45 +0200

    Remade generate method in BiomorphGenerate to use MyCallable in multithread processing

--- BiomorphGenerate/src/BiomorphGenerate.java
+++ BiomorphGenerate/src/BiomorphGenerate.java
@@ -7,6 +7,13 @@ import org.apache.commons.math3.util.FastMath;
 
 import java.awt.*;
 
+import java.util.*;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class BiomorphGenerate
 {
@@ -18,6 +25,10 @@ public class BiomorphGenerate
     private MyPoint point_a;
     private MyPoint point_b;
     private int magCheck = 1;
+
+    private static final int NTHREDS = 10;
+
+
 
     public BiomorphGenerate()
     {
@@ -40,9 +51,67 @@ public class BiomorphGenerate
         return false;
     }
 
+    class MyCallable implements Callable<Integer> {
+        private int output;
+        private Iterate innerMethod;
+        private double threshold;
+        private int iterations;
+        private MyPoint point_a;
+        private MyPoint point_b;
+        private double step;
+        private int i;
+        private int j;
+
+        public MyCallable(
+                int i,
+                int j,
+                Iterate method,
+                double step,
+                MyPoint point_a,
+                MyPoint point_b,
+                double threshold,
+                int iterations)
+        {
+            this.i = i;
+            this.j = j;
+            innerMethod = method;
+            this.threshold = threshold;
+            this.iterations = iterations;
+            this.point_a = point_a;
+            this.point_b = point_b;
+            this.step = step;
+        }
+
+        @Override
+        public Integer call() throws Exception {
+
+            method.setInput(new Complex(i*step+point_a.GetX(),j*step-point_a.GetY()));
+
+            output = -1;
+            for (int l = 0; l<iterations;l++)
+            {
+                method.Calculate(true);
+                if (MagnitudeCheck(method.getInput(),magCheck))
+                {
+                    output = l;
+                    break;
+                }
+            }
+            if (output != -1)
+                if ((FastMath.abs(method.getInput().getReal())<threshold||FastMath.abs(method.getInput().getImaginary())<threshold))
+                    output = -1;
+
+            return output;
+        }
+
+    }
+
     private void generate()
     {
         double step = FastMath.abs(point_a.GetX()-point_b.GetX())/size;
+
+        ExecutorService executor = Executors.newFixedThreadPool(NTHREDS);
+        List<Future<Integer>> list = new ArrayList<Future<Integer>>();
 
         int[][] tmp = new int[size][size];
 
@@ -50,8 +119,12 @@ public class BiomorphGenerate
         {
             for (int j = 0; j<size; j++)
             {
+                Callable<Integer> worker = new MyCallable(i,j,method,step, point_a, point_b,threshold,iterations);
+                Future<Integer> submit = executor.submit(worker);
+                list.add(submit);
+
                 //method.setInput(new Complex(i*step-centring-view_control.getOffset_x(),j*step-centring-view_control.getOffset_y()));
-                method.setInput(new Complex(i*step+point_a.GetX(),j*step-point_a.GetY()));
+                /*method.setInput(new Complex(i*step+point_a.GetX(),j*step-point_a.GetY()));
 
                 tmp[i][j] = -1;
                 for (int l = 0; l<iterations;l++)
@@ -65,7 +138,19 @@ public class BiomorphGenerate
                 }
                 if (tmp[i][j] != -1)
                     if ((FastMath.abs(method.getInput().getReal())<threshold||FastMath.abs(method.getInput().getImaginary())<threshold))
-                        tmp[i][j] = -1;
+                        tmp[i][j] = -1;*/
+            }
+        }
+
+        int counter = 0;
+        for (Future<Integer> future : list)
+        {
+            try {
+                tmp[counter%size][(int)FastMath.floor(counter/size)] = future.get();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
             }
         }
     }
